b0d43e6b7294c7688c4dc41ebd3ce20106efa9c8
marlinbit <marlinjosefeliz@gmail.com>
2025-07-17 21:45:39 -0400

    Add files via upload

---
+++ datosf/DataManager.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace datosf
+{
+    public class DataManager
+    {
+        //PASO 1: Estabeler Cadena de Conexion con la Base de Datos
+        private SqlConnection Conexion = new SqlConnection("data source = MARLIN; Database=EmpresaMarlin; Integrated Security=True;");
+        //private SqlConnection Conexion = new SqlConnection("server=localhost\\SQLEXPRESS; Initial Catalog=University; Integrated Security=True;");
+        //private SqlConnection Conexion = new SqlConnection("server=.\\SQLEXPRESS; Initial Catalog=University; Integrated Security=True;");
+        //private SqlConnection Conexion = new SqlConnection("server=127.0.0.1\\SQLEXPRESS; Initial Catalog=University; Integrated Security=True;");
+        // private SqlConnection Conexion = new SqlConnection("server=DESKTOP-57G3KOL\\SQLEXPRESS; Initial Catalog=University; Integrated Security=True; Connection Timeout=30;");
+
+
+        //PASO 2: Crear Metodos para Abrir y Cerrar conexion con la Base de Datos
+
+        public SqlConnection Conectar() // Metodo utilizado para abrir conexion o conectar con la base de datos
+        {
+            if (Conexion.State == ConnectionState.Closed)
+            {
+                Conexion.Open();
+            }
+            return Conexion;
+        }
+
+        public SqlConnection Cerrar() // Metodo utilizado para cerrar conexion o conectar con la base de datos
+        {
+            if (Conexion.State == ConnectionState.Open)
+            {
+                Conexion.Close();
+            }
+            return Conexion;
+        }
+
+        //PASO 3: Crear Metodos para ejecutar comandos SQL desde nuestra aplicacion
+
+        public void Ejecutar(string sql)
+        {
+            SqlCommand oComando = new SqlCommand();
+            oComando.Connection = Conexion;
+            oComando.CommandText = sql;
+            oComando.ExecuteNonQuery();
+
+        }
+
+        public void Ejecutar1(string sql, ref int numgenerado)
+        {
+            SqlCommand oComando = new SqlCommand();
+            oComando.Connection = Conexion;
+            oComando.CommandText = sql;
+            numgenerado = Convert.ToInt32(oComando.ExecuteNonQuery());
+
+        }
+
+        //PASO 4: Crear Metodo para recuperar datos desde la base de datos y manejarlos como tabla virtual
+
+        public DataTable Buscar(string sql)
+        {
+            DataTable dt = new DataTable();
+            SqlDataAdapter oAdapter = new SqlDataAdapter();
+            SqlCommand oComando = new SqlCommand();
+
+            oComando.Connection = Conexion;
+            oComando.CommandText = sql;
+            oAdapter.SelectCommand = oComando;
+
+            oAdapter.Fill(dt);
+
+            return dt;
+        }
+    }
+}
---
+++ negociossA/Empleado.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using datosf;
+
+
+namespace negociossA
+{
+    public class Empleado
+    { 
+        public DataManager oBD = new DataManager();
+
+        public void InsertarEmpleado(int ID, string nombre, string apellido, string Cedula, string Departamento, string Cargo, string sueldo,
+            String FechaNacimiento, string sexo, string correo, string telefono, String FechaIngreso, string CuentaBancaria)
+        {
+            string sql;
+            oBD.Conectar();
+
+            sql = @"INSERT INTO Empleados (ID, nombre, apellido, Cedula,Departamento, Cargo, sueldo,
+            FechaNacimiento,  sexo,  correo, telefono, FechaIngreso, CuentaBancaria)
+                        VALUES ('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}','{8}','{9}','{10}','{11}','{12}')";
+
+            sql = string.Format(sql, ID, nombre, apellido, Cedula, Departamento, Cargo, sueldo,
+            FechaNacimiento, sexo, correo, telefono, FechaIngreso, CuentaBancaria);
+            oBD.Ejecutar(sql);
+
+            oBD.Cerrar();
+            
+        }
+
+        public void ActualizarEmpleado( int ID, string nombre, string apellido, string Cedula, string Departamento, string Cargo, string sueldo,
+            String  FechaNacimiento, string sexo, string correo, string telefono, String FechaIngreso, string CuentaBancaria)
+        {
+            string sql;
+            oBD.Conectar();
+
+            sql = @"UPDATE Empleados SET nombre='{1}',Apellido='{2}',Cedula='{3}',Departamento='{4}',Cargo='{5}',Sueldo='{6}',Fecha de Nacimiento='{7}',SEXO='{8}',Correo='{9}', 
+                 Correo='{9}', Telefono='{10}',Fecha Ingreso='{11}', Cuenta Bancaria='{12}'  WHERE EmpleadoID='{9}')";
+
+            sql = string.Format(sql, ID, nombre, apellido, Cedula, Departamento, Cargo, sueldo,
+            FechaNacimiento, sexo, correo, telefono, FechaIngreso, CuentaBancaria);
+            oBD.Ejecutar(sql);
+
+            oBD.Cerrar();
+        }
+
+        public void EliminarEmpleado(int Id)
+        {
+            oBD.Conectar();
+
+            string sql = "Delete From Empleados where EmpleadoID= {0}";
+            sql = string.Format(sql, Id);
+
+            oBD.Ejecutar(sql);
+
+            oBD.Cerrar();
+        }
+
+
+        public DataTable CargarEmpleado()
+
+        {
+            System.Data.DataTable dt;
+
+            oBD.Conectar();
+            string sql;
+            sql = @"Select EmpleadoID'Datos', 
+                                Concat (Nombre,' ',Apellido) 'Nombre Completo',Cedula, FechaNacimiento 'Fecha', Telefono,  from Empleados";
+            dt = oBD.Buscar(sql);
+
+            oBD.Cerrar();
+
+            return dt;
+
+        }
+
+    }
+}
+
